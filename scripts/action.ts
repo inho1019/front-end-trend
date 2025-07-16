8e8e6e366882dcd4ff7c093c7fed43ce2a0b5ace
inho1019 <inho1019@naver.com>
2025-07-16 10:03:09 +0900

    chore: SERVICE ANNOUNCEMENT 안뜨게

--- scripts/action.ts
+++ scripts/action.ts
@@ -16,7 +16,7 @@ const parser = new RSSParser();
         const sites = JSON.parse(readFileSync(targetSite, 'utf8')) as Site[];
         const parsing = await Promise.all(sites.map(async (site: Site) => {
             const feed = await parser.parseURL(`${process.env.VITE_RSS_PROXY_URL}${site.url}`);
-            const items = feed.items || [];
+            const items = feed.items.filter(item => (item[site.type.title] || item.title) !== "SERVICE ANNOUNCEMENT: About this feed") || [];
             const parsedData: ParserData[] = items.map(item => {
                 const createdRaw = item[site.type.createdAt];
                 const content = item[site.type.content];
