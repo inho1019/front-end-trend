5182b3b72ca0457057766bb2682f5fd1f674e394
inho1019 <inho1019@naver.com>
2025-08-04 14:24:26 +0900

    fix: 사이트 수정

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const parser = new RSSParser();
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL(`${process.env.VITE_RSS_PROXY_URL}https://oliveyoung.tech/rss.xml`);
+    const feed = await parser.parseURL(`${process.env.VITE_RSS_PROXY_URL}`);
 
     console.log(feed);
     
