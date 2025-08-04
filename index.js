5c6c416e25a9127ba6d18585093b50ce9920a32f
inho1019 <inho1019@naver.com>
2025-08-04 13:58:03 +0900

    chore: data delete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const parser = new RSSParser();
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL(`${process.env.VITE_RSS_PROXY_URL}https://feeds.feedburner.com/gaeraeblog`);
+    const feed = await parser.parseURL(`${process.env.VITE_RSS_PROXY_URL}https://oliveyoung.tech/rss.xml`);
 
     console.log(feed);
     
