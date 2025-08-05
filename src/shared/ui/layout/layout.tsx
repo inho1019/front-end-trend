4e38cf8d098a99b0fb8877985fc376dd67ff2289
inho1019 <inho1019@naver.com>
2025-08-05 13:25:14 +0900

    chore: 스크롤 초기화 로직 삽입

--- src/shared/ui/layout/layout.tsx
+++ src/shared/ui/layout/layout.tsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { ToggleTransButton } from "../google-translate";
+import { useEffect, useRef } from "react";
 
 export const Layout = () => {
+    const { pathname } = useLocation();
+    const layoutRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (layoutRef.current) {
+            layoutRef.current.scrollTo(0, 0);
+        }
+    }, [pathname])
+
     return (
-        <div className="overflow-y-auto flex flex-col h-screen">
+        <div ref={layoutRef} className="overflow-y-auto flex flex-col h-screen">
             <Outlet />
             <ToggleTransButton className="fixed bottom-20 left-20 z-50 opacity-80 active:opacity-60 transition-opacity" />
         </div>
