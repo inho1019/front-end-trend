9247239264305cca8f71153ea4edbba2b648aca4
inho_m <120188554+inho1019@users.noreply.github.com>
2025-08-14 05:03:20 +0900

    trend-item.tsx 업데이트

--- src/features/trend/ui/trend-item.tsx
+++ src/features/trend/ui/trend-item.tsx
@@ -18,7 +18,7 @@ export const TrendItem = ({ data, ...props }: TrendItemProps) => {
     }, [data.site.id, siteData])
 
     return (
-        <div {...props} draggable={false} className={twMerge("space-y-4", props.className)}>
+        <div {...props} draggable={false} className={twMerge("space-y-5", props.className)}>
             <div className="flex flex-row gap-5">
                 <div className="flex justify-center items-center text-xs size-16 rounded-xs overflow-hidden bg-gray-100 dark:bg-[#222]">
                     <img src={image ?? Logo} className="size-full object-cover" onError={e => e.currentTarget.src = Logo} />
@@ -26,7 +26,7 @@ export const TrendItem = ({ data, ...props }: TrendItemProps) => {
                 <p className="text-xs text-gray-500 font-medium">{data.site.name}</p> 
             </div>
             <h2 className="text-xl font-semibold line-clamp-2 leading-24 max-sm:text-lg max-sm:leading-22">{data.title}</h2>
-            <div className="mt-16 flex text-[10px] text-gray-400">
+            <div className="mt-10 flex text-[10px] text-gray-400">
           <p className="max-w-1/2 truncate">{data?.author || "Unknown"}</p><p>&nbsp;|&nbsp;{DateTime.fromISO(data.createdAt).toFormat("yyyy. MM. dd")}</p>
 
 </div> 
