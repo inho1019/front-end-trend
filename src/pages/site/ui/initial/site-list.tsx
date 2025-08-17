be1cff3fd23e0e64d30f20f010bac13b162fd11a
inho_m <120188554+inho1019@users.noreply.github.com>
2025-08-17 15:08:40 +0900

    site-list.tsx 업데이트

--- src/pages/site/ui/initial/site-list.tsx
+++ src/pages/site/ui/initial/site-list.tsx
@@ -23,7 +23,7 @@ export const SiteList = () => {
                                         data={item}
                                         to={item.link ?? item.url}
                                         target="_blank"
-                                        className="transition active:not-[&:has(button:hover)]:bg-gray-50 active:not-[&:has(button:hover)]:dark:bg-[#222] px-5 py-15 min-sm:hover:not-active:not-[&:has(button:hover)]:opacity-80"
+                                        className="transition active:not-[&:has(button:hover)]:bg-gray-50 active:not-[&:has(button:hover)]:dark:bg-[#222] px-5 py-15 max-sm:py-10 min-sm:hover:not-active:not-[&:has(button:hover)]:opacity-80"
                                     />
                                 ))
                             }
