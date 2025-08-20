8764f14096120590b13e937c80870909a3f6d655
inho1019 <inho1019@naver.com>
2025-08-20 14:05:14 +0900

    chore: 디자인 개선

--- src/shared/ui/layout/footer.tsx
+++ src/shared/ui/layout/footer.tsx
@@ -4,7 +4,7 @@ import { Button } from "../common";
 
 export const Footer = () => {
     return (
-        <footer className="shrink-0 h-100 border-t border-t-gray-200 mx-10 flex flex-col gap-5 items-center justify-center max-sm:h-80">
+        <footer className="shrink-0 h-120 mx-10 flex flex-col gap-5 items-center justify-center max-sm:h-100">
             <div className="flex flex-row gap-10">
                 <Link to="https://github.com/inho1019/front-end-trend" className="size-24 bg-white rounded-sm flex items-center justify-center active:opacity-70 transition-opacity max-sm:size-22" draggable="false" target="_blank" rel="noopener noreferrer">
                     <GithubIcon />
