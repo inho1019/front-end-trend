2ceabaa2d5255d8290036c3365eb0f0757835135
inho1019 <inho1019@naver.com>
2025-08-20 18:39:01 +0900

    chore: css 수정

--- src/features/trend/ui/trend-search.tsx
+++ src/features/trend/ui/trend-search.tsx
@@ -32,7 +32,7 @@ export const TrendSearch = () => {
             aria-expanded={scrollTop > 15}
             className={
                 twMerge(
-                    "relative transition-all duration-500 text-black flex flex-1 flex-row gap-5 items-center max-w-250 rounded-full bg-gray-100 py-4 pl-29 pr-12 dark:bg-[#222] dark:text-white min-2xl:max-w-480",
+                    "relative transition-all duration-500 text-black flex flex-1 flex-row gap-5 items-center max-w-250 rounded-full bg-gray-100 py-4 pl-29 pr-12 dark:bg-[#222] dark:text-white min-2xl:max-w-480 min-2xl:duration-700",
                     "aria-expanded:max-w-full aria-expanded:min-2xl:max-w-full aria-expanded:bg-white dark:aria-expanded:bg-dark aria-expanded:gap-10 aria-expanded:pl-20 max-sm:aria-expanded:pl-10 aria-expanded:pr-0 min-sm:aria-expanded:ml-10",
             )}>
             <div aria-hidden={scrollTop < 16} className="absolute left-0 h-20 w-1 bg-current aria-hidden:opacity-0"/>
