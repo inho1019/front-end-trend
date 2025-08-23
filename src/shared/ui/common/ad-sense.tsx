c3c19b4f9245ffa415790baa9ee3845e257450f2
inho_m <120188554+inho1019@users.noreply.github.com>
2025-08-23 19:34:44 +0900

    ad-sense.tsx 업데이트

--- src/shared/ui/common/ad-sense.tsx
+++ src/shared/ui/common/ad-sense.tsx
@@ -22,8 +22,8 @@ export const AdSense = () => {
   return (
     <ins
         ref={adRef}
-        className="adsbygoogle"
-        style={{ display: "block", height: "100px", margin: "10px 5px" }}
+        className="adsbygoogle my-5"
+        style={{ display: "block", height: "100px" }}
         data-ad-client="ca-pub-9982505674721509"
         data-ad-slot="9256101892"
         data-ad-format="fluid"
