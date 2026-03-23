4291a9ed27675a9dc4182e54b9a12aadf5fbdb91
inho1019 <inho1019@naver.com>
2026-03-23 15:31:40 +0900

    chore: asdf

--- src/features/trend/ui/trend-panel.tsx
+++ src/features/trend/ui/trend-panel.tsx
@@ -4,7 +4,7 @@ import { useScrollingObserver } from "@shared/lib/screen/use-activating-observer
 import { sanitizeHtml, twMerge, useTrans } from "@shared/lib/utils";
 import type { ParserData } from "@shared/model/parser";
 import { AdSense, Button, Spinner } from "@shared/ui/common";
-import { Panel } from "@shared/ui/panel"
+import { Panel } from "@shared/ui/panel";
 import { DateTime } from "luxon";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "react-router";
@@ -14,190 +14,227 @@ import { useScreen } from "@shared/lib/screen";
 import { useCodePanel } from "@features/code";
 
 export interface TrendPanelProps {
-    data: ParserData | null;
-    isOpen: boolean;
-    onClose: () => void;
+  data: ParserData | null;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-export const TrendPanel = ({ data, isOpen, onClose }: TrendPanelProps) => { 
-    const viewerRef = useRef<HTMLDivElement>(null);
-    const panelRef = useRef<HTMLDivElement>(null);
-    const detailsRef = useRef<HTMLDetailsElement>(null);
+export const TrendPanel = ({ data, isOpen, onClose }: TrendPanelProps) => {
+  const viewerRef = useRef<HTMLDivElement>(null);
+  const panelRef = useRef<HTMLDivElement>(null);
+  const detailsRef = useRef<HTMLDetailsElement>(null);
 
-    useScrollingObserver(viewerRef, [data]);
+  useScrollingObserver(viewerRef, [data]);
 
-    const { isOpen: isCodeOpen, isHidden } = useCodePanel();
+  const { isOpen: isCodeOpen, isHidden } = useCodePanel();
 
-    const [aiSummaryContent, setAiSummaryContent] = useState("")
-    const [aiSummaryStep, setAiSummaryStep] = useState<"pending" | "loading" | "complete" | "error">("pending")
+  const [aiSummaryContent, setAiSummaryContent] = useState("");
+  const [aiSummaryStep, setAiSummaryStep] = useState<
+    "pending" | "loading" | "complete" | "error"
+  >("pending");
 
-    const trans = useTrans();
+  const trans = useTrans();
 
-    const { addMessage } = useMessage();
-    const { scrolling } = useScreen();
+  const { addMessage } = useMessage();
+  const { scrolling } = useScreen();
 
-    const aiContent = useMemo(() => ({ __html: aiSummaryContent }), [aiSummaryContent]);
-    const htmlContent = useMemo(() => ({ __html: data ? sanitizeHtml(data.content) : ""}), [data]);
+  const aiContent = useMemo(
+    () => ({ __html: aiSummaryContent }),
+    [aiSummaryContent],
+  );
+  const htmlContent = useMemo(
+    () => ({ __html: data ? sanitizeHtml(data.content) : "" }),
+    [data],
+  );
 
-    const handleAiSummary = useCallback(async () => {
-        if (!data?.content) return "";
-        const tempDiv = document.createElement("div");
-        tempDiv.innerHTML = data.content;
-        try {
-            setAiSummaryStep("loading")
-            const response = await getAiSummary(data.title, data.createdAt, data?.link ?? "", tempDiv.textContent || tempDiv.innerText);
-            setAiSummaryContent(response ?? "")
-            setAiSummaryStep("complete")
-        } catch (error) {
-            console.log(error)
-            setAiSummaryStep("error");
-        }
-    }, [data]);
+  const handleAiSummary = useCallback(async () => {
+    if (!data?.content) return "";
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = data.content;
+    try {
+      setAiSummaryStep("loading");
+      const response = await getAiSummary(
+        data.title,
+        data.createdAt,
+        data?.link ?? "",
+        tempDiv.textContent || tempDiv.innerText,
+      );
+      setAiSummaryContent(response ?? "");
+      setAiSummaryStep("complete");
+    } catch (error) {
+      console.log(error);
+      setAiSummaryStep("error");
+    }
+  }, [data]);
 
-    const reset = useCallback(() => {
-        setAiSummaryContent("");
-        setAiSummaryStep("pending");
-        if (detailsRef.current) {
-            detailsRef.current.open = false
-        }
-    }, [])
+  const reset = useCallback(() => {
+    setAiSummaryContent("");
+    setAiSummaryStep("pending");
+    if (detailsRef.current) {
+      detailsRef.current.open = false;
+    }
+  }, []);
 
-    const handleClose = useCallback(() => {
-        reset();
-        onClose();
-    }, [onClose, reset]);
+  const handleClose = useCallback(() => {
+    reset();
+    onClose();
+  }, [onClose, reset]);
 
-    useEffect(() => {
-        if (data) {
-            reset();
-            if (viewerRef.current) {
-                viewerRef.current.scrollTop = 0;
-            }
-        }
-    }, [data, reset]);
+  useEffect(() => {
+    if (data) {
+      reset();
+      if (viewerRef.current) {
+        viewerRef.current.scrollTop = 0;
+      }
+    }
+  }, [data, reset]);
 
-    const handleResize = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
-        const panel = panelRef.current;
-        if (!panel) return;
-        const startX = e.clientX;
-        const startWidth = panel.offsetWidth;
+  const handleResize = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e.preventDefault();
+      const panel = panelRef.current;
+      if (!panel) return;
+      const startX = e.clientX;
+      const startWidth = panel.offsetWidth;
 
-        const onMouseMove = (moveEvent: MouseEvent) => {
-            const delta = moveEvent.clientX - startX;
-            let newWidth = startWidth - delta;
-            newWidth = Math.max(640, Math.min(newWidth, window.innerWidth));
-            panel.style.width = `${newWidth}px`;
-        };
+      const onMouseMove = (moveEvent: MouseEvent) => {
+        const delta = moveEvent.clientX - startX;
+        let newWidth = startWidth - delta;
+        newWidth = Math.max(640, Math.min(newWidth, window.innerWidth));
+        panel.style.width = `${newWidth}px`;
+      };
 
-        const onMouseUp = () => {
-            window.removeEventListener("mousemove", onMouseMove);
-            window.removeEventListener("mouseup", onMouseUp);
-        };
+      const onMouseUp = () => {
+        window.removeEventListener("mousemove", onMouseMove);
+        window.removeEventListener("mouseup", onMouseUp);
+      };
 
-        window.addEventListener("mousemove", onMouseMove);
-        window.addEventListener("mouseup", onMouseUp);
-    }, []);
+      window.addEventListener("mousemove", onMouseMove);
+      window.addEventListener("mouseup", onMouseUp);
+    },
+    [],
+  );
 
-    useEffect(() => {
-        const copyMessage = () => addMessage(trans("trend.copyCode", "코드 복사 완료"));
-        window.addEventListener("copy-message", copyMessage);
-        return () => window.removeEventListener("copy-message", copyMessage);
-    }, [addMessage, trans]);
+  useEffect(() => {
+    const copyMessage = () =>
+      addMessage(trans("trend.copyCode", "코드 복사 완료"));
+    window.addEventListener("copy-message", copyMessage);
+    return () => window.removeEventListener("copy-message", copyMessage);
+  }, [addMessage, trans]);
 
-    if (!data) return null;
+  ////////
 
-    return (
-        <Panel
-            ref={panelRef}
-            isOpen={isOpen}
-            position="right"
-            className="h-[calc(100%-50px)] w-640 top-50 py-5 px-10 max-sm:px-5 max-sm:w-full max-sm:max-w-640 min-sm:min-w-640 min-sm:max-w-full"
+  if (!data) return null;
+
+  return (
+    <Panel
+      ref={panelRef}
+      isOpen={isOpen}
+      position="right"
+      className="h-[calc(100%-50px)] w-640 top-50 py-5 px-10 max-sm:px-5 max-sm:w-full max-sm:max-w-640 min-sm:min-w-640 min-sm:max-w-full"
+    >
+      <div className="relative rounded-xl flex flex-col bg-white p-15 h-full dark:bg-dark panel-shadow">
+        <div className="space-y-5 pb-10 border-b border-b-gray-200 dark:border-b-[#666]">
+          <div className="flex flex-row justify-between gap-5">
+            <details open className="group">
+              <summary className="text-xl font-semibold group-open:line-clamp-2 group-open:max-sm:line-clamp-1 break-all">
+                {data?.title}
+              </summary>
+            </details>
+            <Button className="self-start" onClick={handleClose}>
+              <XIcon />
+            </Button>
+          </div>
+          <div className="flex flex-row items-center">
+            <p className="text-xs font-medium">{data.site.name}&nbsp;•&nbsp;</p>
+            <Link
+              to={data?.link ?? "#"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex-1 cursor-pointer text-xs text-blue-500 underline hover:text-blue-700 transition-colors line-clamp-1 break-all"
+            >
+              {data?.link}
+            </Link>
+          </div>
+        </div>
+        <div
+          ref={viewerRef}
+          className="flex-1 overflow-y-auto pt-15 space-y-20 max-sm:pb-40"
         >
-            <div className="relative rounded-xl flex flex-col bg-white p-15 h-full dark:bg-dark panel-shadow">
-                <div className="space-y-5 pb-10 border-b border-b-gray-200 dark:border-b-[#666]">
-                    <div className="flex flex-row justify-between gap-5">
-                        <details open className="group">
-                           <summary className="text-xl font-semibold group-open:line-clamp-2 group-open:max-sm:line-clamp-1 break-all">{data?.title}</summary>
-                        </details>
-                        <Button className="self-start" onClick={handleClose}>
-                            <XIcon />
-                        </Button>
-                    </div>
-                    <div className="flex flex-row items-center">
-                        <p className="text-xs font-medium">{data.site.name}&nbsp;•&nbsp;</p>
-                        <Link
-                            to={data?.link ?? "#"}
-                            target="_blank" 
-                            rel="noopener noreferrer" 
-                            className="flex-1 cursor-pointer text-xs text-blue-500 underline hover:text-blue-700 transition-colors line-clamp-1 break-all"
-                        >
-                            {data?.link}
-                        </Link>
-                    </div>
-                </div>
-                <div ref={viewerRef} className="flex-1 overflow-y-auto pt-15 space-y-20 max-sm:pb-40">
-                    <details ref={detailsRef} open={false} className="transition-colors duration-300 p-15 bg-gray-50 rounded-lg dark:bg-[#222] group not-open:active:bg-gray-100 dark:not-open:active:bg-[#333]">
-                        <summary onClick={() => aiSummaryStep === "pending" && handleAiSummary()}  className="flex flex-row justify-between items-center cursor-pointer px-5">
-                            <div className="flex flex-row items-center gap-2 text-sm font-semibold">
-                                <AiSummaryIcon />
-                                {trans("trend.ai.summary", "요약")}
-                            </div>
-                            <div className="size-fit transition-transform duration-300 rotate-270 group-open:rotate-90">
-                                <ArrowIcon />
-                            </div>
-                        </summary>
-                        <div className="mt-15 space-y-10">
-                            {(() => {
-                                switch (aiSummaryStep) {
-                                    case "loading":
-                                        return <Spinner className="size-48 border-7 mx-auto mb-30 mt-45" />;
-                                    case "complete":
-                                        return (
-                                            <>
-                                                <div
-                                                    className="viewer whitespace-pre-wrap animate-fade"
-                                                    dangerouslySetInnerHTML={aiContent}
-                                                />
-                                                <AdSense />
-                                            </>
-                                        )
-                                    case "error":
-                                        return (
-                                                <p className="text-sm font-light mx-auto mb-30 mt-45 h-48 flex justify-center items-center">
-                                                    {trans("trend.ai.error", "요약 생성에 실패했습니다.")}
-                                                </p>
-                                            );
-                                    default :
-                                    return null;
-                                }
-                            })()}
-                        </div>
-                    </details>
-                    <div
-                        className="viewer whitespace-pre-wrap"
-                        dangerouslySetInnerHTML={htmlContent}
-                    />
-                </div>
-                <ScrollToTopButton 
-                    scrollRef={viewerRef} 
-                    aria-disabled={!isOpen || scrolling || (isCodeOpen && !isHidden)}
-                    className={twMerge("transition-opacity duration-300 ease-out fixed bottom-116 right-15 z-50 active:opacity-70 group-aria-hidden:opacity-0 aria-disabled:opacity-0 aria-disabled:pointer-events-none min-sm:hidden")}
-                />
-                <Button
-                    className={twMerge(
-                        "flex items-center text-gray-400 cursor-col-resize transition-colors duration-300 absolute w-15 h-full top-0 -left-0 rounded-l-xl max-sm:hidden",
-                        "bg-linear-to-r from-white to-white dark:from-dark dark:to-dark active:from-gray-100/100 active:to-gray-100/0 dark:active:from-gray-800/100 dark:active:to-gray-800/0" 
-                    )}
-                    onMouseDown={handleResize}
-                >
-                    <DragIcon />    
-                </Button>
-                <div className="absolute w-[calc(100%-30px)] flex flex-row items-center bottom-0 text-[10px] justify-end font-medium max-sm:justify-start">
-                    <div>{data?.author || "Unknown"}</div>&nbsp;•&nbsp;
-                    <div className="shrink-0">{DateTime.fromISO(data.createdAt).toFormat("yyyy. MM. dd")}</div>
-                </div>
+          <details
+            ref={detailsRef}
+            open={false}
+            className="transition-colors duration-300 p-15 bg-gray-50 rounded-lg dark:bg-[#222] group not-open:active:bg-gray-100 dark:not-open:active:bg-[#333]"
+          >
+            <summary
+              onClick={() => aiSummaryStep === "pending" && handleAiSummary()}
+              className="flex flex-row justify-between items-center cursor-pointer px-5"
+            >
+              <div className="flex flex-row items-center gap-2 text-sm font-semibold">
+                <AiSummaryIcon />
+                {trans("trend.ai.summary", "요약")}
+              </div>
+              <div className="size-fit transition-transform duration-300 rotate-270 group-open:rotate-90">
+                <ArrowIcon />
+              </div>
+            </summary>
+            <div className="mt-15 space-y-10">
+              {(() => {
+                switch (aiSummaryStep) {
+                  case "loading":
+                    return (
+                      <Spinner className="size-48 border-7 mx-auto mb-30 mt-45" />
+                    );
+                  case "complete":
+                    return (
+                      <>
+                        <div
+                          className="viewer whitespace-pre-wrap animate-fade"
+                          dangerouslySetInnerHTML={aiContent}
+                        />
+                        <AdSense />
+                      </>
+                    );
+                  case "error":
+                    return (
+                      <p className="text-sm font-light mx-auto mb-30 mt-45 h-48 flex justify-center items-center">
+                        {trans("trend.ai.error", "요약 생성에 실패했습니다.")}
+                      </p>
+                    );
+                  default:
+                    return null;
+                }
+              })()}
             </div>
-        </Panel>
-    )
-}
+          </details>
+          <div
+            className="viewer whitespace-pre-wrap"
+            dangerouslySetInnerHTML={htmlContent}
+          />
+        </div>
+        <ScrollToTopButton
+          scrollRef={viewerRef}
+          aria-disabled={!isOpen || scrolling || (isCodeOpen && !isHidden)}
+          className={twMerge(
+            "transition-opacity duration-300 ease-out fixed bottom-116 right-15 z-50 active:opacity-70 group-aria-hidden:opacity-0 aria-disabled:opacity-0 aria-disabled:pointer-events-none min-sm:hidden",
+          )}
+        />
+        <Button
+          className={twMerge(
+            "flex items-center text-gray-400 cursor-col-resize transition-colors duration-300 absolute w-15 h-full top-0 -left-0 rounded-l-xl max-sm:hidden",
+            "bg-linear-to-r from-white to-white dark:from-dark dark:to-dark active:from-gray-100/100 active:to-gray-100/0 dark:active:from-gray-800/100 dark:active:to-gray-800/0",
+          )}
+          onMouseDown={handleResize}
+        >
+          <DragIcon />
+        </Button>
+        <div className="absolute w-[calc(100%-30px)] flex flex-row items-center bottom-0 text-[10px] justify-end font-medium max-sm:justify-start">
+          <div>{data?.author || "Unknown"}</div>&nbsp;•&nbsp;
+          <div className="shrink-0">
+            {DateTime.fromISO(data.createdAt).toFormat("yyyy. MM. dd")}
+          </div>
+        </div>
+      </div>
+    </Panel>
+  );
+};
